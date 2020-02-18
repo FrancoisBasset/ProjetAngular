fd6f074898fb1e6f337614d5da0123667d7aed88
jeremie <jeremieroland@orange.fr>
2020-02-18 15:16:13 +0100

    merge tests jeremie

---
+++ src/logic/models/Pokemon.spec.ts
@@ -0,0 +1,50 @@
+import { Pokemon } from ".";
+import Attack from './Attack';
+
+describe('Pokemon logic model', () => {
+  let pikachu: Pokemon;
+  let charge: Attack;
+  let MathRandom: () => number;
+
+  beforeEach( () => {
+    charge = new Attack('Charge', 5);
+    pikachu = new Pokemon(
+        'Pikachu',
+        70,
+        90,
+        55,
+        40,
+        30,
+        [
+          new Attack('Eclair', 40),
+          charge,
+          new Attack('Tonnerre', 60),
+          new Attack('Rugissement', 1)
+        ],
+        [],
+        []
+      )
+
+      MathRandom = Math.random;
+      Math.random = () => 0.01;
+  });
+
+  it('Pikachu should have 20HP after having lost 10HP', () => {
+    pikachu.loseHealth(10);
+    expect(pikachu.health).toBe(20);
+  });
+
+  it('Pikachu should have 0HP after having lost 99HP', () => {
+    pikachu.loseHealth(99);
+    expect(pikachu.health).toBe(0);
+  });
+
+  it('Pikachu get 2nd attack : Charge', () => {
+    let attack = pikachu.getRandomAttack();
+    expect(attack).toBe(charge);
+  });
+
+  afterEach( () => {
+      Math.random = MathRandom;
+  })
+});
