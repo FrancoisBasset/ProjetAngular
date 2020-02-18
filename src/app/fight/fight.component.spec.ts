103cc1bc349fb6d9a8a9711aa09420aee6702e1e
jeremie <jeremieroland@orange.fr>
2020-02-18 15:14:18 +0100

    tests Pokemon

--- src/app/fight/fight.component.spec.ts
+++ src/app/fight/fight.component.spec.ts
@@ -5,6 +5,7 @@ import { FightComponent } from './fight.component';
 describe('FightComponent', () => {
   let component: FightComponent;
   let fixture: ComponentFixture<FightComponent>;
+  let view: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +17,19 @@ describe('FightComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(FightComponent);
     component = fixture.componentInstance;
+    view = fixture.elementRef.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  /*it('should render "Pikachu"', () => {
+    expect(view.innerHTML).toContain('Pikachu');
+  });
+
+  it('should render "Ditto"', () => {
+    expect(view.innerHTML).toContain('Ditto');
+  });*/
 });
