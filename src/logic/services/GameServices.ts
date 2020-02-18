8db9fa7ecb2b2b2a978d4543144169bb67961d75
jeremie <jeremieroland@orange.fr>
2020-02-18 14:08:40 +0100

    rounds code amélioré

--- src/logic/services/GameServices.ts
+++ src/logic/services/GameServices.ts
@@ -3,36 +3,38 @@ import AttackServices from "./AttackServices";
 import SpeedServices from './SpeedServices';
 
 export default class GameServices {
+    intervalId: NodeJS.Timeout;
+    fastestToAttack = true;
 
     constructor(
         public write: (message: string) => void,
-        public a: Pokemon,
-        public b: Pokemon,
+        public pokemonA: Pokemon,
+        public pokemonB: Pokemon,
         public attackServices: AttackServices,
         public speedServices: SpeedServices
     )
     {}
 
+    private demiRound() {
+        let fastest = this.speedServices.getFastest(this.pokemonA, this.pokemonB)
+        let attacker = ( fastest === this.pokemonA  && this.fastestToAttack ? this.pokemonA : this.pokemonB )
+        let target = ( attacker === this.pokemonA ? this.pokemonB : this.pokemonA )
+
+        let attack = attacker.getRandomAttack();
+        let dammages = this.attackServices.attack(attacker, attack, target)
+        this.write(`${attacker.name} utilise l'attaque ${attack.name}`);
+        this.write(`${target.name} perd ${dammages} (${target.health} PV restants)`)
+
+        if ( target.health <= 0 ) {
+            this.write(`${target.name} n'a plus de PV`)
+            this.write(`${attacker.name} remporte le combat`)
+            clearInterval(this.intervalId);
+        }
+
+        this.fastestToAttack = !this.fastestToAttack;
+    }
+
     public play () {
-        let fastestToAttack = true;
-        let intervalId = setInterval( () => {
-            let fastest = this.speedServices.getFastest(this.a, this.b)
-            let attacker = ( fastest === this.a  && fastestToAttack ? this.a : this.b )
-            let target = ( attacker === this.a ? this.b : this.a )
-
-            let attack = attacker.getRandomAttack();
-            let dammages = this.attackServices.attack(attacker, attack, target)
-            this.write(`${attacker.name} utilise l'attaque ${attack.name}`);
-            this.write(`${target.name} perd ${dammages} (${target.health} PV restants)`)
-
-            if ( target.health <= 0 ) {
-                this.write(`${target.name} n'a plus de PV`)
-                this.write(`${attacker.name} remporte le combat`)
-                clearInterval(intervalId);
-                return
-            }
-
-            fastestToAttack = !fastestToAttack;
-        }, 1000);
+        this.intervalId = setInterval(this.demiRound, 1000);
     }
 }
